940564f0eb3b27bb904d5782922f55031a4139b3
Administrator <Administrator@N301PC23>
2024-10-16 21:50:46 +0700

    Add project files.

---
+++ Exercise_02/Program.cs
@@ -0,0 +1,51 @@
+﻿
+internal class Program
+{
+    public static void Main()
+    {
+        //ex_01();
+        //ex_02();
+        //ex_03();
+        Console.ReadKey();
+    }
+    public static void ex_01()
+    {
+        //đổi độ C sang độ K và độ F
+        Console.Write("Nhap vao do C: ");
+        float c = float.Parse(Console.ReadLine());
+        double k = c + 273;
+        double f = c * 18 / 10 + 32;
+        Console.WriteLine($"Chuyen thanh do K: {k}");
+        Console.WriteLine($"Chuyen thanh do F: {f}");
+    }
+    public static void ex_02()
+    {
+        //tính diện tích bề mặt và thể tích
+        Console.Write("Nhap vao ban kinh: ");
+        float a = float.Parse(Console.ReadLine());
+        double pi = Math.PI;
+        double s = 4 * pi * Math.Pow(a, 2);
+        double v = 4 / 3 * pi * Math.Pow(a, 3);
+        Console.WriteLine($"Dien tich be mat la: {s}");
+        Console.WriteLine($"The tich la: {v}");
+    }
+    public static void ex_03()
+    {
+        Console.Write("Nhap vao so thu nhat: ");
+        float a = float.Parse(Console.ReadLine());
+        Console.Write("Nhap vao so thu hai: ");
+        float b = float.Parse(Console.ReadLine());
+        double cong = a + b;
+        double tru = a - b;
+        double nhan = a * b;
+        double chia = a / b;
+        double chiadu = a % b;
+        Console.WriteLine($"Tong 2 so: {cong}");
+        Console.WriteLine($"Hieu 2 so: {tru}");
+        Console.WriteLine($"Tich 2 so: {nhan}");
+        Console.WriteLine($"Thuong 2 so (nguyen): {chia}");
+        Console.WriteLine($"Thuong 2 so (du): {chiadu}");
+    }
+
+
+}
